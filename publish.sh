491a1d9b8a6032d8380482a64499a6cc2d495789
ThibaultLatrille <thibault.latrille@univ-lyon1.fr>
2018-11-16 18:16:59 +0100

    google-scholar social icons

--- publish.sh
+++ publish.sh
@@ -3,5 +3,7 @@ rm -rf ./public/
 hugo
 cd ./public/
 git init
+git add .
+git commit -m "Publishing"
 git remote add origin git@github.com:ThibaultLatrille/ThibaultLatrille.github.io.git
 git push --force --set-upstream origin master
